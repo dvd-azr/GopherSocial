4ffcd6a85ed79d5fd261b185fd46c6af8be74d22
david Azhari <46072244+dvd-azr@users.noreply.github.com>
2024-09-17 02:35:47 +0700

    Update main.go pg-port localhost

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -44,7 +44,8 @@ func main() {
 			// addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
 			
 
-			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@db:5432/socialnetwork?sslmode=disable"),
+			// addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@db:5432/socialnetwork?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost:5432/socialnetwork?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
